bd93bb8dfa7f11345ce1bd138f71312e8a812096
mjdubose <matthew.dubose@gmail.com>
2016-09-24 14:40:21 -0500

    First commit

---
+++ StringUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace ThirtyDaysofTDD.UnitTests
+{
+    class StringUtils
+    {
+        public int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            var stringToCheckAsCharacterArray = sentenceToScan.ToCharArray();
+            var characterToCheckFor = char.Parse(characterToScanFor);
+
+            return stringToCheckAsCharacterArray.Count(currentchar => currentchar == characterToCheckFor);
+        }
+    }
+}
---
+++ StringUtilsTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace ThirtyDaysofTDD.UnitTests
+{
+    [TestFixture]
+    public class StringUtilsTest {
+
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+            var sentenceToScan = "TDD is awesome!";
+            var characterToScanFor = "e";
+            var expectedResult = 2;
+
+            var stringUtils = new StringUtils();
+
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult,result);
+
+        }
+
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInAComplexSentence()
+        {
+            var sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern";
+            var characterToScanFor = "n";
+            var expectedResult = 5;
+            var stringUtils = new StringUtils();
+
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult,result);
+
+        }
+    }
+}
